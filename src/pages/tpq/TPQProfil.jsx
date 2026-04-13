4ebdcbc9b51af74596f3cfce92f021ce505869a3
tktpq-al-huda <tktpqalhuda@gmail.com>
2026-04-13 18:42:43 +0700

    minor update

--- src/pages/tpq/TPQProfil.jsx
+++ src/pages/tpq/TPQProfil.jsx
@@ -68,7 +68,7 @@ export default function TPQProfil() {
             {/* Kotak Motto */}
             <div className="bg-huda-green/10 border-l-8 border-huda-green p-6 rounded-r-3xl mb-8">
               <Quote className="text-huda-green w-10 h-10 mb-3 opacity-50" />
-              <h4 className="text-3xl font-extrabold text-huda-dark italic leading-tight">" Generasi Qur'ani dan Berakhlak Mulia"</h4>
+              <h4 className="text-3xl font-extrabold text-huda-dark italic leading-tight">"Generasi Qur'ani dan Berakhlak Mulia"</h4>
               <p className="text-gray-600 font-bold mt-2">- Moto TPQ Al-Huda</p>
             </div>
 
